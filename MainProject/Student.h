3dbab31db81df105da157750ccd0f4a16353cdec
zodiaaccc <freseyyy@gmail.com>
2025-04-07 17:38:22 +0300

    Use structual object

--- MainProject/Student.h
+++ MainProject/Student.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Student {
@@ -8,4 +9,14 @@ public:
 	int age;
 	double mark;
 	bool alive;
+ 
+	string getString() {
+		string s = "Name: " + name + "\n";
+		s += "Age: " + to_string(age) + "\n";
+		s += "Mark: " + to_string(mark) + "\n";
+		s += "Alive: ";
+		s += (alive ? "Yes" : "No");
+		return s + "\n";
+	}
+
 };
--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,20 +1,40 @@
 #include "Student.h"
 
+
+void change(Student student) {
+	student.name = "Vova";
+	student.age = 15;
+	student.mark = 10;
+	student.alive = false;
+}
+
 int main() {
-	Student st1, st2;
+	Student st1, temp;
 
 	st1.name = "Bogdan";
 	st1.age = 14;
-	st1.mark - 9.5;
+	st1.mark = 9.5;
 	st1.alive = true;
 
-	st2.name = "Ivan";
+	/*st2.name = "Ivan";
 	st2.age = 15;
-	st2.mark - 4.5;
-	st2.alive = true;
+	st2.mark = 4.5;
+	st2.alive = true;*/
 
-	//string name = st1.mark > st2.mark ? st1.name : st2.name;
-	Student t = st1.mark > st2.mark ? st1 : st2;
-	cout << "Best student is " << t.name << endl;
+
+	temp = st1;
+
+	cout << "Before: " << endl;
+	cout << st1.getString() << endl;
+	cout << temp.getString() << endl;
+
+	/*change(st1);*/
+	st1.name = "Vova";
+
+	cout << "After: " << endl;
+	cout << st1.getString() << endl;
+	cout << temp.getString() << endl;
+	
+
 	return 0;
 }
