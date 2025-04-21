cb189c9606dc38a82054b4e54dd840259c7a23a8
zodiaaccc <freseyyy@gmail.com>
2025-04-21 18:54:38 +0300

    all

--- MainProject/Student.h
+++ MainProject/Student.h
@@ -7,13 +7,15 @@ class Student {
 public:
 	string name;
 	int age;
-	double mark;
+	int* marks;
+	int countMarks;
 	bool alive;
    //Default-constructor or construsctor without arguments
 	Student() {
 		name = "no name";
 		age = 13;
-		mark = 4.0;
+		marks = nullptr;
+		countMarks = 0;
 		alive = true;
 	}
 	//construsctor with arguments
@@ -24,33 +26,60 @@ public:
 		alive = true;
 	}
 	//canonical construsctor 
-	Student(string nm, int a, double m, bool al) {
-		name = nm;
-		age = a;
-		mark = m;
-		alive = al;
+	Student(string name, int age, double mark, bool alive) {
+		this ->name = name;
+		this->age = age;
+		this->alive = alive;
+		marks = new int[countMarks];
+		for (int i = 0; i < countMarks; i++)
+		{
+			marks[i] = 4;
+		}
 	}
 
 	//copy construsctor 
 	Student(const Student& student) {
-		cout << "copycopycopy";
+		//cout << "copycopycopy";
 		name = student.name;
 		age = student.age;
 		mark = student.mark;
 		alive = student.alive;
 	}
-	//Destuctor
+	//Destuctor	
 	~Student() {
-		cout << "destructor.." << endl;
-		
+	 //cout << "destructor.." << endl;
+		if (marks != nullptr) {
+			delete[] marks;
+		}
+
 	}
 	string toString() {
 		string s = "Name: " + name + "\n";
 		s += "Age: " + to_string(age) + "\n";
-		s += "Mark: " + to_string(mark) + "\n";
+		s += "count of marks: " + to_string(countMarks) + "\n";
 		s += "Alive: ";
 		s += (alive ? "Yes" : "No");
 		return s + "\n";
 	}
+	string get_all_marks() {
+		if (countMarks == 0)
+		{
+			return "[]";
+		}
+		string s = "";
+		for (int i = 0; i < countMarks; i++)
+		{
+			string += to_string(marks[i]) + " ";
+		}
+		return s;
+	}
+	int getMark(int index) {
+		return index < 0 || index >= countMarks ? 0 : marks[index];
+	}
+	void setMark(int index) {
+		if (index >= 0 ||)
+		{
 
+		}
+	}
 };
--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,16 +1,13 @@
 #include "Teacher.h"
 
+int a;
 int main() {
-	Student student1{};
-	Student student2{ "Alex" };
-	Student student3("Vlad", 13, 10, true);
-	Student student4{ student3 };
+	Student st1("Alex", 15 , 4.0, true);
+	for (int i = 0; i < countMark; i++)
+	{
 
-	cout << student1.toString() << endl;
-	cout << student2.toString() << endl;
-	cout << student3.toString() << endl;
-	cout << student4.toString() << endl;
-
+	}
+	cout << st1.toString() << endl;
 
 	return 0;
 }
